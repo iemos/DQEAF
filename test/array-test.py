db30ceb7e88810d4cc046eb3c9ef30ce9e22f839
fangzhiyang <fanmcgrady@gmail.com>
2018-01-18 12:48:38 +0800

    修改test
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

---
+++ test/array-test.py
@@ -0,0 +1,11 @@
+import chainer.links as L
+import chainer.functions as F
+
+net = []
+n_hidden_channels = [1024, 256]
+inpdim = 4
+for i, n_hid in enumerate(n_hidden_channels):
+    net += [('l{}'.format(i), inpdim, n_hid)]
+    net += [('norm{}'.format(i), n_hid)]
+    net += [('_act{}'.format(i), 0)]
+print(net)
