21f254fa33f46e34d9bfcd92a24b05e7ccf2fbb3
fangzhiyang <fanmcgrady@gmail.com>
2018-01-20 15:11:56 +0800

    修改test
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- test/peFeature.py
+++ test/peFeature.py
@@ -3,6 +3,13 @@ from gym_malware.envs.utils import interface
 from gym_malware.envs.utils.pefeatures import PEFeatureExtractor
 
 bytez = interface.fetch_file("VirusShare_0390d1a31da40afa7641efad8ca672d4")
-print(bytez)
+# print(bytez)
 features = PEFeatureExtractor().extract(bytez)
+print(features.__len__())
+print(features)
+
+bytez = interface.fetch_file("VirusShare_0b3c009aa4e461a00c0b3755976b485e")
+# print(bytez)
+features = PEFeatureExtractor().extract(bytez)
+print(features.__len__())
 print(features)
