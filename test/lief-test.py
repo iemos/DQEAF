dfe3159193e05dd88065bb00cf3047a8f2eac70d
fangzhiyang <fanmcgrady@gmail.com>
2018-01-25 22:07:49 +0800

    添加lief-tutorials
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- test/lief-test.py
+++ test/lief-test.py
@@ -1,13 +1,22 @@
 import lief
+
 from gym_malware.envs.utils import interface
 
+# 从文件名parse
 file_path = interface.get_sample_real_path("VirusShare_616a0554aa4821545c4a0aa1e89420ba")
 binary = lief.parse(file_path)
-print(binary.imports)
+print(binary)
+print("-----------------------------")
 
+# 从bytez字节parse，该方法只能使用lief 0.7版本
+# Mac系统下使用0.8要报错，原因未知
+binary1 = lief.PE.parse(interface.fetch_file("VirusShare_616a0554aa4821545c4a0aa1e89420ba"))
+print(binary1)
+
+######################
 for imported_library in binary.imports:
-  print("Library name: " + imported_library)
-  for func in imported_library.entries:
-    if not func.is_ordinal:
-      print(func.name)
-    print(func.iat_address)
+    print("Library name: " + imported_library)
+    for func in imported_library.entries:
+        if not func.is_ordinal:
+            print(func.name)
+        print(func.iat_address)
