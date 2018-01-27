4b0c390d405e0741c133d83f50d4c474c278b095
fangzhiyang <fanmcgrady@gmail.com>
2018-01-27 18:11:20 +0800

    添加trainer
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- train_agent_kerasrl.py
+++ train_agent_kerasrl.py
@@ -2,6 +2,7 @@ import pickle
 
 import gym
 import numpy as np
+import sys
 
 np.random.seed(123)  # set a random seed when setting up the gym environment (train_test_split)
 
@@ -79,6 +80,13 @@ def train_dqn_model(layers, rounds=10000, run_test=False, use_score=False):
 
 
 if __name__ == '__main__':
+    # python train_agent_kerasrl.py dqn ...
+    # function_name = sys.argv[1]
+    # if function_name == 'dqn':
+    #     # ...
+    # else:
+    #     # ...
+
     agent1, model1, history_train1, history_test1 = train_dqn_model([1024, 256], rounds=50000, run_test=True,
                                                                     use_score=False)  # black blox
     model1.save('models/dqn.h5', overwrite=True)
