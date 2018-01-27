9afcc79fc0533997ae997e62a4c2c7fead346eb9
fangzhiyang <fanmcgrady@gmail.com>
2018-01-27 13:22:44 +0800

    添加trainer
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- bin/trainer.py
+++ bin/trainer.py
@@ -1,7 +1,7 @@
-import bin.train_agent_chainer as tac
+from train_agent_chainer import *
 
-tac.train_agent(rounds=5000, use_score=True, name='models/create_ddqn_agent_score_5000',
-                create_agent=tac.create_ddqn_agent)  # allow graduation_agent to see scores
+train_agent(rounds=5000, use_score=True, name='models/create_ddqn_agent_score_5000',
+            create_agent=create_ddqn_agent)  # allow graduation_agent to see scores
 
-tac.train_agent(rounds=5000, use_score=False, name='models/create_ddqn_agent_5000',
-                create_agent=tac.create_ddqn_agent)  # black blox
+train_agent(rounds=5000, use_score=False, name='models/create_ddqn_agent_5000',
+            create_agent=create_ddqn_agent)  # black blox
