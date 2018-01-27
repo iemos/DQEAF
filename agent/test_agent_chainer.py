032e2cef8b8e1fb614d058a461dafb2d3ef91825
fangzhiyang <fanmcgrady@gmail.com>
2018-01-27 11:41:31 +0800

    添加trainer
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- agent/test_agent_chainer.py
+++ agent/test_agent_chainer.py
@@ -8,10 +8,8 @@ from gym_malware.envs.utils import interface, pefeatures
 
 ACTION_LOOKUP = {i: act for i, act in enumerate(manipulate.ACTION_TABLE.keys())}
 
-from train_agent_chainer import create_acer_agent
-from train_agent_chainer import create_ddqn_agent
+from agent.train_agent_chainer import create_ddqn_agent
 import gym
-import gym_malware
 
 
 def evaluate(action_function):
---
+++ bin/train.py
@@ -0,0 +1,7 @@
+from agent import train_agent_chainer as tac
+
+tac.train_agent(rounds=5000, use_score=True, name='models/create_ddqn_agent_score_5000',
+                create_agent=tac.create_ddqn_agent)  # allow agent to see scores
+
+tac.train_agent(rounds=5000, use_score=False, name='models/create_ddqn_agent_5000',
+                create_agent=tac.create_ddqn_agent)  # black blox
