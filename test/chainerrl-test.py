28332891f2be4eb7ff502b82f96cb1772ad7ced7
fanmcgrady <fanmcgrady@gmail.com>
2018-02-04 12:27:31 +0800

    添加冯同学的修改部分

--- test/chainerrl-test.py
+++ test/chainerrl-test.py
@@ -30,7 +30,7 @@ obs_size = env.observation_space.shape[0]
 n_actions = env.action_space.n
 q_func = QFunction(obs_size, n_actions)
 # use ChainerRL's predefined Q-functions
-#q_func = chainerrl.q_functions.FCStateQFunctionWithDiscreteAction(
+# q_func = chainerrl.q_functions.FCStateQFunctionWithDiscreteAction(
 #     obs_size, n_actions,
 #     n_hidden_layers=2, n_hidden_channels=50)
 # q_func.to_gpu()
@@ -60,59 +60,59 @@ agent = chainerrl.agents.DoubleDQN(
     replay_start_size=500, update_interval=1,
     target_update_interval=100, phi=phi)
 
-# train agent
-n_episodes = 300
-max_episode_len = 200
-for i in range(1, n_episodes + 1):
-    obs = env.reset()
-    reward = 0
-    done = False
-    R = 0  # return (sum of rewards)
-    t = 0  # time step
-    while not done and t < max_episode_len:
-        # Uncomment to watch the behaviour
-        # env.render()
-        action = agent.act_and_train(obs, reward)
-        obs, reward, done, _ = env.step(action)
-        R += reward
-        t += 1
-    if i % 10 == 0:
-        print('episode:', i,
-              'R:', R)
-    agent.stop_episode_and_train(obs, reward, done)
-print('Agent Finished.')
-
-# test agent
-for i in range(10):
-    obs = env.reset()
-    done = False
-    R = 0
-    t = 0
-    while not done and t < max_episode_len:
-#        env.render()
-        action = agent.act(obs)
-        obs, r, done, _ = env.step(action)
-        R += r
-        t += 1
-    print('test episode:', i, 'R:', R)
-    agent.stop_episode()
-print('RL completed!')
-
-agent.save("cart")
-print("Agent cart saved!")
+# # train agent
+# n_episodes = 300
+# max_episode_len = 200
+# for i in range(1, n_episodes + 1):
+#     obs = env.reset()
+#     reward = 0
+#     done = False
+#     R = 0  # return (sum of rewards)
+#     t = 0  # time step
+#     while not done and t < max_episode_len:
+#         # Uncomment to watch the behaviour
+#         # env.render()
+#         action = agent.act_and_train(obs, reward)
+#         obs, reward, done, _ = env.step(action)
+#         R += reward
+#         t += 1
+#     if i % 10 == 0:
+#         print('episode:', i,
+#               'R:', R)
+#     agent.stop_episode_and_train(obs, reward, done)
+# print('Agent Finished.')
+#
+# # test agent
+# for i in range(10):
+#     obs = env.reset()
+#     done = False
+#     R = 0
+#     t = 0
+#     while not done and t < max_episode_len:
+# #        env.render()
+#         action = agent.act(obs)
+#         obs, r, done, _ = env.step(action)
+#         R += r
+#         t += 1
+#     print('test episode:', i, 'R:', R)
+#     agent.stop_episode()
+# print('RL completed!')
+#
+# agent.save("cart")
+# print("Agent cart saved!")
 
 
 # Set up the logger to print info messages for understandability.
-# import logging
-# import sys
-# gym.undo_logger_setup()  # Turn off gym's default logger settings
-# logging.basicConfig(level=logging.INFO, stream=sys.stdout, format='')
-#
-# chainerrl.experiments.train_agent_with_evaluation(
-#     agent, env,
-#     steps=200,           # Train the graduation_agent for 2000 steps
-#     eval_n_runs=10,       # 10 episodes are sampled for each evaluation
-#     max_episode_len=200,  # Maximum length of each episodes
-#     eval_interval=100,   # Evaluate the graduation_agent after every 1000 steps
-#     outdir='result')      # Save everything to 'result' directory
+import logging
+import sys
 
+gym.undo_logger_setup()  # Turn off gym's default logger settings
+logging.basicConfig(level=logging.INFO, stream=sys.stdout, format='')
+
+chainerrl.experiments.train_agent_with_evaluation(
+    agent, env,
+    steps=2000,  # Train the graduation_agent for 2000 steps
+    eval_n_runs=10,  # 10 episodes are sampled for each evaluation
+    max_episode_len=200,  # Maximum length of each episodes
+    eval_interval=1000,  # Evaluate the graduation_agent after every 1000 steps
+    outdir='result')  # Save everything to 'result' directory
