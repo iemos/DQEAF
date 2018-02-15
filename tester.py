d03556425952e52d48fe258c8e8840d4fa5de0db
fangzhiyang <fanmcgrady@gmail.com>
2018-02-15 17:17:00 +0800

    kerasrl-tutorials
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- tester.py
+++ tester.py
@@ -1,12 +1,24 @@
 #! /usr/bin/python
+import argparse
+import datetime
 
-import sys
 from bin.test_agent_chainer import test_models
-import bin.utils as util
-
+from bin.train_agent_chainer import *
 
 model_dir = "models/"
-model_saved_name, rounds, agent_method = util.get_args3(sys.argv)
+timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M")
+parser = argparse.ArgumentParser()
+parser.add_argument('--model-name', type=str, default=timestamp)
+parser.add_argument('--rounds', type=int, default=50000)
+parser.add_argument('--agent', choices=['dqn', 'acer'], default='dqn')
+args = parser.parse_args()
+
+model_saved_name = args.model_name
+rounds = args.rounds
+if args.agent == "dqn":
+    agent_method = create_ddqn_agent
+else:
+    agent_method = create_acer_agent
 
 score_model = "{}{}_score_{}".format(model_dir, model_saved_name, rounds)
 model = "{}{}_{}".format(model_dir, model_saved_name, rounds)
--- trainer.py
+++ trainer.py
@@ -1,16 +1,29 @@
 #! /usr/bin/python
+import argparse
+import datetime
+
 from bin.train_agent_chainer import *
-import bin.utils as util
-import sys
 
 # 用于快速调用chainerrl的训练方法，参数如下：
-# python trainer.py name rounds dqn(or acer)
+# python trainer.py --model-name name --rounds rounds --agent dqn(or acer)
 # 1. 保存的模型名称
 # 2. 训练的次数
 # 3. 使用的创建agent方法
 
 model_dir = "models/"
-model_saved_name, rounds, agent_method = util.get_args3(sys.argv)
+timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M")
+parser = argparse.ArgumentParser()
+parser.add_argument('--model-name', type=str, default=timestamp)
+parser.add_argument('--rounds', type=int, default=50000)
+parser.add_argument('--agent', choices=['dqn', 'acer'], default='dqn')
+args = parser.parse_args()
+
+model_saved_name = args.model_name
+rounds = args.rounds
+if args.agent == "dqn":
+    agent_method = create_ddqn_agent
+else:
+    agent_method = create_acer_agent
 
 score_model = "{}{}_score_{}".format(model_dir, model_saved_name, rounds)
 model = "{}{}_{}".format(model_dir, model_saved_name, rounds)
