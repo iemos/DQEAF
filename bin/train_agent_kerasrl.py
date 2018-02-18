0ef51d94b92c52010a7fe71d7e41814b41a7cf7c
fangzhiyang <fanmcgrady@gmail.com>
2018-02-18 22:53:30 +0800

    kerasrl-tutorials
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- bin/train_agent_kerasrl.py
+++ bin/train_agent_kerasrl.py
@@ -2,7 +2,6 @@ import pickle
 
 import gym
 import numpy as np
-import sys
 import gym_malware
 
 np.random.seed(123)  # set a random seed when setting up the gym environment (train_test_split)
@@ -88,8 +87,8 @@ if __name__ == '__main__':
     with open('history_blackbox.pickle', 'wb') as f:
         pickle.dump(history_test1, f, pickle.HIGHEST_PROTOCOL)
 
-    agent2, model2, history_train2, history_test2 = train_dqn_model([1024, 256], rounds=50000, run_test=True,
-                                                                    use_score=True)  # allow graduation_agent to see scores
-    model2.save('models/dqn_score.h5', overwrite=True)
-    with open('history_score.pickle', 'wb') as f:
-        pickle.dump(history_test2, f, pickle.HIGHEST_PROTOCOL)
+        # agent2, model2, history_train2, history_test2 = train_dqn_model([1024, 256], rounds=50000, run_test=True,
+        #                                                                 use_score=True)  # allow graduation_agent to see scores
+        # model2.save('models/dqn_score.h5', overwrite=True)
+        # with open('history_score.pickle', 'wb') as f:
+        #     pickle.dump(history_test2, f, pickle.HIGHEST_PROTOCOL)
