cf7727db2dc4e2dc6fdb50daff82efe3532504ba
fangzhiyang <fanmcgrady@gmail.com>
2018-02-24 11:11:28 +0800

    kerasrl-tutorials
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- trainer.py
+++ trainer.py
@@ -14,7 +14,7 @@ model_dir = "models/"
 timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M")
 parser = argparse.ArgumentParser()
 parser.add_argument('--model-name', type=str, default=timestamp)
-parser.add_argument('--rounds', type=int, default=50000)
+parser.add_argument('--rounds', type=int, default=10000)
 parser.add_argument('--agent', choices=['dqn', 'acer'], default='dqn')
 args = parser.parse_args()
 
