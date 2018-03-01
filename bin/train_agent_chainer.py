a428b26f92c3e9048f147ce72fe7b91eae735d97
fangzhiyang <fanmcgrady@gmail.com>
2018-03-01 11:09:16 +0800

    样本预处理
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- bin/train_agent_chainer.py
+++ bin/train_agent_chainer.py
@@ -3,7 +3,6 @@ import chainer.functions as F
 import chainer.links as L
 import chainerrl
 import gym
-import gym_malware
 import numpy as np
 from chainerrl import links
 from chainerrl.action_value import DiscreteActionValue
@@ -136,7 +135,7 @@ def train_agent(rounds=10000, use_score=False, name='result_dir', create_agent=c
     ENV_NAME = 'malware-score-v0' if use_score else 'malware-v0'
     env = gym.make(ENV_NAME)
     np.random.seed(123)
-    env.seed(123)
+    # env.seed(123)
 
     agent = create_agent(env)
 
