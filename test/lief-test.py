72a146194daa62e1ec1553424ca478a88e5fe73a
fangzhiyang <fanmcgrady@gmail.com>
2018-03-17 14:45:40 +0800

    尝试将action修改为增加不同长度的随机字符到末尾
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- test/lief-test.py
+++ test/lief-test.py
@@ -3,20 +3,22 @@ import lief
 from gym_malware.envs.utils import interface
 
 # 从文件名parse
-file_path = interface.get_sample_real_path("Backdoor.Win32.Hupigon.zah")
-binary = lief.parse(file_path)
-print(binary)
-print("-----------------------------")
+# file_path = interface.get_sample_real_path("Backdoor.Win32.Hupigon.zah")
+# binary = lief.parse(file_path)
+# print(binary)
+# print("-----------------------------")
 
 # 从bytez字节parse，该方法只能使用lief 0.7版本
 # Mac系统下使用0.8要报错，原因未知
-binary1 = lief.PE.parse(interface.fetch_file("Backdoor.Win32.Hupigon.zah"))
+binary1 = lief.PE.parse(interface.fetch_file("Backdoor.Win32.PcClient.geq"))
+# binary1 = lief.PE.parse(interface.fetch_file("Backdoor.Win32.PcClient.gdv"))
 print(binary1)
+print(binary1.optional_header.checksum)
 
 ######################
-for imported_library in binary.imports:
-    print("Library name: ", imported_library)
-    for func in imported_library.entries:
-        if not func.is_ordinal:
-            print(func.name)
-        print(func.iat_address)
+# for imported_library in binary.imports:
+#     print("Library name: ", imported_library)
+#     for func in imported_library.entries:
+#         if not func.is_ordinal:
+#             print(func.name)
+#         print(func.iat_address)
