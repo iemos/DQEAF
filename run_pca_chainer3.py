b7ca4b944b0b9d06995ac241c85be46a3c6faee2
fangzhiyang <fanmcgrady@gmail.com>
2018-04-26 23:12:42 +0800

    冯同学的PCA版本
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

---
+++ run_pca_chainer3.py
@@ -0,0 +1,54 @@
+#! /usr/bin/python
+import argparse
+import datetime
+
+from bin.test_agent_pca_chainer3 import test_models
+from bin.train_agent_pca_chainer3 import *
+from gym_malware.envs.controls import manipulate2 as manipulate
+
+# 用于快速调用chainerrl的训练方法，参数如下：
+# python run_chainer.py --model-name name --rounds rounds --agent dqn(or acer)
+# 1. 保存的模型名称
+# 2. 训练的次数
+# 3. 使用的创建agent方法
+
+model_dir = "models3/"
+timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M")
+parser = argparse.ArgumentParser()
+parser.add_argument('--model-name', type=str, default=timestamp)
+parser.add_argument('--rounds', type=int, default=10000)
+args = parser.parse_args()
+
+model_saved_name = args.model_name
+rounds = args.rounds
+agent_method = create_ddqn_agent
+
+score_model = "{}{}_score_{}".format(model_dir, model_saved_name, rounds)
+model = "{}{}_{}".format(model_dir, model_saved_name, rounds)
+test_result = "{}{}_test_result.txt".format(model_dir, model_saved_name)
+
+# start time
+training_start_time = datetime.datetime.now()
+with open(test_result, 'a+') as f:
+    f.write("training started: {}\n".format(training_start_time))
+
+# allow graduation_agent to see scores
+# train_agent(rounds=int(rounds), use_score=True, name=score_model, create_agent=agent_method)
+
+# black blox
+train_agent(rounds=int(rounds), use_score=False, name=model, create_agent=agent_method)
+
+training_end_time = datetime.datetime.now()
+with open(test_result, 'a+') as f:
+    f.write("training end: {}\n".format(training_end_time))
+    f.write("action:")
+    for key in manipulate.ACTION_TABLE:
+        f.write(key)
+        f.write(',')
+    f.write("\n")
+
+# test
+for i in range(3):
+    with open(test_result, 'a+') as f:
+        result1, result2, result3 = test_models(model, score_model, agent_method)
+        f.write("{}:{}, {}, {}\n".format(i, result1, result2, result3))
