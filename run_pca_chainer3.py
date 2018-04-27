1cf2e429ab5326f51ba7d4e208eff3dbb3a9e5e7
jessie <610891637@qq.com>
2018-04-27 17:00:34 +0800

    pca

--- run_pca_chainer3.py
+++ run_pca_chainer3.py
@@ -10,7 +10,7 @@ model_dir = "models3/"
 timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M")
 parser = argparse.ArgumentParser()
 parser.add_argument('--model-name', type=str, default=timestamp)
-parser.add_argument('--rounds', type=int, default=10000)
+parser.add_argument('--rounds', type=int, default=1)
 args = parser.parse_args()
 
 model_saved_name = args.model_name
