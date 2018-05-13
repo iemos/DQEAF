e19d43796910c7d88f83ecc39fe7f77db8699d57
fangzhiyang <fanmcgrady@gmail.com>
2018-05-13 16:11:50 +0800

    update
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- test_kerasrl.py
+++ test_kerasrl.py
@@ -1,5 +1,25 @@
-from bin.test_agent_kerasrl import test_models
+import argparse
+import datetime
 
-if __name__ == '__main__':
-    result1, result2, result3 = test_models('bin/models/dqn.h5', '', False)
-    print("{}, {}, {}\n".format(result1, result2, result3))
+import pickle
+
+from bin.test_agent_kerasrl import test_models
+from bin.train_agent_kerasrl import train_dqn_model
+from gym_malware.envs.controls import manipulate2 as manipulate
+
+# timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M")
+timestamp = '201805130852'
+parser = argparse.ArgumentParser()
+parser.add_argument('--rounds', type=int, default=10000)
+args = parser.parse_args()
+
+rounds = args.rounds
+
+model_name = 'models/kerasrl_{}_{}.h5'.format(timestamp, rounds)
+test_result = "models/kerasrl_{}_{}.txt".format(timestamp, rounds)
+
+# test
+for i in range(1):
+    with open(test_result, 'a+') as f:
+        result1, result2, result3 = test_models(model_name, '')
+        f.write("{}:{}, {}, {}\n".format(i, result1, result2, result3))
