9a4174dca7b39ebcd1c8a4a7175118dfa5310426
fangzhiyang <fanmcgrady@gmail.com>
2018-05-15 20:50:36 +0800

    update
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- test/lief-test.py
+++ test/lief-test.py
@@ -5,19 +5,6 @@ from gym_malware.envs.controls import manipulate2 as manipulate
 
 # 从bytez字节parse，该方法只能使用lief 0.7版本
 # Mac系统下使用0.8要报错，原因未知
-byte = interface.fetch_file("Backdoor.Win32.PcClient.geq");
+byte = interface.fetch_file("Backdoor.Win32.Hupigon.zay");
 binary = lief.PE.parse(byte)
-print(binary.imported_functions)
-
-man = manipulate.MalwareManipulator(byte)
-binary2 = lief.PE.parse(man.imports_append_org())
-print(binary2.imported_functions)
-
-######################
-# for imported_library in binary.imports:
-#     print("Library name: ", imported_library)
-#     for func in imported_library.entries:
-#         if not func.is_ordinal:
-#             print(func.name)
-#         print(func.iat_address)
-
+print(binary)
