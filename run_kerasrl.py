9965a33936d0333acacff46d584f63ced0c74974
fangzhiyang <fanmcgrady@gmail.com>
2018-05-15 20:50:36 +0800

    update
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- run_kerasrl.py
+++ run_kerasrl.py
@@ -2,12 +2,11 @@ import argparse
 import datetime
 
 from bin.test_agent_kerasrl import test_models
-from bin.train_agent_kerasrl import train_dqn_model
-from gym_malware.envs.controls import manipulate2 as manipulate
+from bin.train_agent_kerasrl import train_dqn_model_EpsGreedy_Policy
 
 timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M")
 parser = argparse.ArgumentParser()
-parser.add_argument('--rounds', type=int, default=3000)
+parser.add_argument('--rounds', type=int, default=10000)
 args = parser.parse_args()
 
 rounds = args.rounds
@@ -23,13 +22,13 @@ with open(test_result, 'a+') as f:
 
 # black blox
 
-_, model1, _, _ = train_dqn_model([256, 256], rounds=rounds)  # black box
+_, model1, _, _ = train_dqn_model_EpsGreedy_Policy([512, 128], rounds=rounds)  # black box
 model1.save(model_name, overwrite=True)
 
 # with open('history_blackbox_{}_{}.pickle'.format(timestamp, rounds), 'wb') as f:
 #     pickle.dump(history_test1, f, pickle.HIGHEST_PROTOCOL)
 
-_, model2, _, _ = train_dqn_model([256, 256], rounds=rounds, use_score=True)  # score
+_, model2, _, _ = train_dqn_model_EpsGreedy_Policy([512, 128], rounds=rounds, use_score=True)  # score
 model2.save(model_score_name, overwrite=True)
 
 training_end_time = datetime.datetime.now()
