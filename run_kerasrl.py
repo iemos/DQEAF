ce31948223185e7ca7b09cdd6a7a85752698b244
fangzhiyang <fanmcgrady@gmail.com>
2018-05-16 23:24:25 +0800

    update
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- run_kerasrl.py
+++ run_kerasrl.py
@@ -22,13 +22,13 @@ with open(test_result, 'a+') as f:
 
 # black blox
 
-_, model1, _, _ = train_dqn_model_EpsGreedy_Policy([512, 128], rounds=rounds)  # black box
+_, model1, _, _ = train_dqn_model_EpsGreedy_Policy([256, 128, 64], rounds=rounds)  # black box
 model1.save(model_name, overwrite=True)
 
 # with open('history_blackbox_{}_{}.pickle'.format(timestamp, rounds), 'wb') as f:
 #     pickle.dump(history_test1, f, pickle.HIGHEST_PROTOCOL)
 
-_, model2, _, _ = train_dqn_model_EpsGreedy_Policy([512, 128], rounds=rounds, use_score=True)  # score
+_, model2, _, _ = train_dqn_model_EpsGreedy_Policy([256, 128, 64], rounds=rounds, use_score=True)  # score
 model2.save(model_score_name, overwrite=True)
 
 training_end_time = datetime.datetime.now()
