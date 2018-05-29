f0a7a26d6feedc335365f5ab3463ddfa054f0b73
fangzhiyang <fanmcgrady@gmail.com>
2018-05-29 12:15:57 +0800

    update
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- test/test.py
+++ test/test.py
@@ -1,15 +1,36 @@
 import numpy as np
+from visdom import Visdom
 
+viz = Visdom()
+viz.text("hello world")
 
-# 一维数组归一化
-def oneDimNormalization(data):
-    min = data.min()
-    max = data.max()
-    range = max - min
-    return (data - min) / range
+Y = np.random.rand(100)
+old_scatter = viz.scatter(
+    X=np.random.rand(100, 2),
+    Y=(Y[Y > 0] + 1.5).astype(int),
+    opts=dict(
+        legend=['Didnt', 'Update'],
+        xtickmin=-50,
+        xtickmax=50,
+        xtickstep=0.5,
+        ytickmin=-50,
+        ytickmax=50,
+        ytickstep=0.5,
+        markersymbol='cross-thin-open',
+        title='plots1'
+    ),
+)
 
-
-arr = np.array([2, 4, 6])
-print(oneDimNormalization(arr))
-
-print("11", "22")
+viz.update_window_opts(
+    win=old_scatter,
+    opts=dict(
+        legend=['Apples', 'Pears'],
+        xtickmin=0,
+        xtickmax=1,
+        xtickstep=0.5,
+        ytickmin=0,
+        ytickmax=1,
+        ytickstep=0.5,
+        markersymbol='cross-thin-open',
+    ),
+)
