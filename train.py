e4d1d9753b9afc12d895257d3e33003fa15a774d
fangzhiyang <fanmcgrady@gmail.com>
2018-06-02 00:31:58 +0800

    完美的模型
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- train.py
+++ train.py
@@ -4,20 +4,47 @@ import argparse
 import datetime
 import os
 
-from bin.test_agent_chainer import evaluate
 from bin.train_agent_chainer import *
-from gym_malware import sha256_holdout
 from gym_malware.envs.controls import manipulate2 as manipulate
-from gym_malware.envs.utils import pefeatures
 
 ACTION_LOOKUP = {i: act for i, act in enumerate(manipulate.ACTION_TABLE.keys())}
+
+q_hook = VisdomPlotHook('Average Q Value')
+loss_hook = VisdomPlotHook('Average Loss', plot_index=1)
+
+
+# 开始训练
+def train_agent(rounds=10000, use_score=False, name='result_dir', create_agent=create_ddqn_agent):
+    ENV_NAME = 'malware-score-v0' if use_score else 'malware-v0'
+    env = gym.make(ENV_NAME)
+    ENV_TEST_NAME = 'malware-score-test-v0' if use_score else 'malware-test-v0'
+    test_env = gym.make(ENV_TEST_NAME)
+    np.random.seed(123)
+    env.seed(123)
+
+    agent = create_agent(env)
+
+    chainerrl.experiments.train_agent_with_evaluation(
+        agent, env,
+        steps=rounds,  # Train the graduation_agent for this many rounds steps
+        max_episode_len=env.maxturns,  # Maximum length of each episodes
+        eval_interval=2000,  # Evaluate the graduation_agent after every 1000 steps
+        eval_n_runs=100,  # 100 episodes are sampled for each evaluation
+        outdir=name,  # Save everything to 'result' directory
+        step_hooks=[q_hook, loss_hook],
+        successful_score=7,
+        eval_env=test_env
+    )
+
+    return env, agent
+
 
 # 用于快速调用chainerrl的训练方法，参数如下：
 # python train.py --rounds rounds(训练的次数)
 model_dir = "models/"
 timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M")
 parser = argparse.ArgumentParser()
-parser.add_argument('--rounds', type=int, default=10000)
+parser.add_argument('--rounds', type=int, default=50000)
 args = parser.parse_args()
 
 model_saved_name = timestamp
