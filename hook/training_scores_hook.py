f0586af15a813a15f3e8dec0d4f61a111d73a8ae
fangzhiyang <fanmcgrady@gmail.com>
2018-06-04 01:53:29 +0800

    完美的模型
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- hook/training_scores_hook.py
+++ hook/training_scores_hook.py
@@ -27,13 +27,15 @@ class TrainingScoresHook(StepHook):
         if self.textwindow is None:
             self.textwindow = self.vis.text(content, opts=self.opts)
         else:
-            self.vis.text(content, win=self.textwindow, append=True)
+            self.vis.text(content, win=self.textwindow)
 
     def __call__(self, env, agent, step):
         if step % 100 == 0:
             with open(os.path.join(self.outdir, 'scores.txt'), 'r') as f:
                 lines = f.readlines()  # 把全部数据文件读到一个列表lines中
+                content = []
                 for line in lines:  # 把lines中的数据逐行读取出来
                     list = line.strip('\n').split(
                         '\t')  # 处理逐行数据：strip表示把头尾的'\n'去掉，split表示以空格来分割行数据，然后把处理后的行数据返回到list列表中
-                    self.text('{}\t|\t{}'.format(list[0], list[3]))
+                    content.append(list[3])
+                self.text(",".join(content))
