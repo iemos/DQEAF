a0196040a8c3d59ea052c77a0aa0a03977072872
fangzhiyang <fanmcgrady@gmail.com>
2018-06-03 12:54:54 +0800

    完美的模型
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- train.py
+++ train.py
@@ -1,14 +1,16 @@
 # coding=UTF-8
 # ! /usr/bin/python
 import argparse
-import datetime
 import os
+import sys
 
+import chainerrl
 import gym
+import numpy as np
+from chainer import optimizers
+from chainerrl import experiments, explorers, q_functions, links, replay_buffer, misc
 
 from bin.test_agent_chainer import evaluate
-from bin.train_agent_chainer import *
-from bin.train_agent_chainer import create_ddqn_agent
 from bin.visdom_plot_hook import VisdomPlotHook
 from gym_malware import sha256_holdout
 from gym_malware.envs.controls import manipulate2 as manipulate
@@ -16,27 +18,101 @@ from gym_malware.envs.utils import pefeatures
 
 ACTION_LOOKUP = {i: act for i, act in enumerate(manipulate.ACTION_TABLE.keys())}
 
-q_hook = VisdomPlotHook('Average Q Value')
-loss_hook = VisdomPlotHook('Average Loss', plot_index=1)
+
+# 创建ddqn agent
+def create_ddqn_agent(env, args):
+    obs_size = env.observation_space.shape[0]
+    action_space = env.action_space
+    n_actions = action_space.n
+
+    q_func = q_functions.FCStateQFunctionWithDiscreteAction(
+        obs_size, n_actions,
+        n_hidden_channels=args.n_hidden_channels,
+        n_hidden_layers=args.n_hidden_layers)
+    if args.gpu >= 0:
+        q_func.to_gpu(args.gpu)
+
+    # Use epsilon-greedy for exploration
+    explorer = explorers.LinearDecayEpsilonGreedy(
+        args.start_epsilon, args.end_epsilon, args.final_exploration_steps,
+        action_space.sample)
+    # explorer = explorers.Boltzmann()
+    # explorer = explorers.ConstantEpsilonGreedy(
+    #     epsilon=0.3, random_action_func=env.action_space.sample)
+
+    if args.noisy_net_sigma:
+        links.to_factorized_noisy(q_func)
+        # Turn off explorer
+        explorer = explorers.Greedy()
+
+    # Draw the computational graph and save it in the output directory.
+    if args.gpu < 0:
+        chainerrl.misc.draw_computational_graph(
+            [q_func(np.zeros_like(env.observation_space, dtype=np.float32)[None])],
+            os.path.join(args.outdir, 'model'))
+
+    opt = optimizers.Adam()
+    opt.setup(q_func)
+
+    rbuf_capacity = 5 * 10 ** 5
+    if args.episodic_replay:
+        if args.minibatch_size is None:
+            args.minibatch_size = 4
+        if args.prioritized_replay:
+            betasteps = (args.steps - args.replay_start_size) // args.update_interval
+            rbuf = replay_buffer.PrioritizedEpisodicReplayBuffer(rbuf_capacity, betasteps=betasteps)
+        else:
+            rbuf = replay_buffer.EpisodicReplayBuffer(rbuf_capacity)
+    else:
+        if args.minibatch_size is None:
+            args.minibatch_size = 32
+        if args.prioritized_replay:
+            betasteps = (args.steps - args.replay_start_size) // args.update_interval
+            rbuf = replay_buffer.PrioritizedReplayBuffer(rbuf_capacity, betasteps=betasteps)
+        else:
+            rbuf = replay_buffer.ReplayBuffer(rbuf_capacity)
+
+    # Chainer only accepts numpy.float32 by default, make sure
+    # a converter as a feature extractor function phi.
+    phi = lambda x: x.astype(np.float32, copy=False)
+
+    agent = chainerrl.agents.DoubleDQN(q_func, opt, rbuf, gamma=args.gamma,
+                                       explorer=explorer, replay_start_size=args.replay_start_size,
+                                       target_update_interval=args.target_update_interval,
+                                       update_interval=args.update_interval,
+                                       phi=phi, minibatch_size=args.minibatch_size,
+                                       target_update_method=args.target_update_method,
+                                       soft_update_tau=args.soft_update_tau,
+                                       episodic_update=args.episodic_replay,
+                                       episodic_update_len=16)
+
+    return agent
+
 
 # 开始训练
-def train_agent(rounds=10000, use_score=False, name='result_dir', create_agent=create_ddqn_agent, use_gpu=False):
+def train_agent(args, use_score=False):
     ENV_NAME = 'malware-score-v0' if use_score else 'malware-v0'
     env = gym.make(ENV_NAME)
     ENV_TEST_NAME = 'malware-score-test-v0' if use_score else 'malware-test-v0'
     test_env = gym.make(ENV_TEST_NAME)
-    np.random.seed(123)
-    env.seed(123)
 
-    agent = create_agent(env, use_gpu)
+    np.random.seed(123)
+    env.seed(456)
+    # Set a random seed used in ChainerRL
+    misc.set_random_seed(789)
+
+    agent = create_ddqn_agent(env, args)
+
+    q_hook = VisdomPlotHook('Average Q Value')
+    loss_hook = VisdomPlotHook('Average Loss', plot_index=1)
 
     chainerrl.experiments.train_agent_with_evaluation(
         agent, env,
-        steps=rounds,  # Train the graduation_agent for this many rounds steps
-        max_episode_len=env.maxturns,  # Maximum length of each episodes
-        eval_interval=500,  # Evaluate the graduation_agent after every 1000 steps
-        eval_n_runs=20,  # 100 episodes are sampled for each evaluation
-        outdir=name,  # Save everything to 'result' directory
+        steps=args.steps,  # Train the graduation_agent for this many rounds steps
+        max_episode_len=args.maxturns,  # Maximum length of each episodes
+        eval_interval=args.eval_interval,  # Evaluate the graduation_agent after every 1000 steps
+        eval_n_runs=args.eval_n_runs,  # 100 episodes are sampled for each evaluation
+        outdir=args.outdir,  # Save everything to 'result' directory
         step_hooks=[q_hook, loss_hook],
         successful_score=7,
         eval_env=test_env
@@ -59,55 +135,69 @@ def get_latest_model_dir_from(basedir):
     assert lastmodel >= 0, "No saved models!"
     return os.path.join(basedir, str(lastmodel))
 
+
 # 用于快速调用chainerrl的训练方法，参数如下：
 # python train.py --rounds rounds(训练的次数)
-model_dir = "models/"
-timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M")
-parser = argparse.ArgumentParser()
-parser.add_argument('--model-name', type=str, default=timestamp)
-parser.add_argument('--rounds', type=int, default=50000)
-parser.add_argument('--test', action='store_true')
-parser.add_argument('--gpu', action='store_true')
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--outdir', type=str, default='models')
+    parser.add_argument('--rounds', type=int, default=50000)
+    parser.add_argument('--test', action='store_true')
+    parser.add_argument('--gpu', action='store_true')
+    parser.add_argument('--final-exploration-steps', type=int, default=10 ** 4)
+    parser.add_argument('--start-epsilon', type=float, default=1.0)
+    parser.add_argument('--end-epsilon', type=float, default=0.1)
+    parser.add_argument('--noisy-net-sigma', action='store_true')
+    parser.add_argument('--demo', action='store_true', default=False)
+    parser.add_argument('--load', type=str, default=None)
+    parser.add_argument('--steps', type=int, default=50000)
+    parser.add_argument('--prioritized-replay', action='store_true', default=True)
+    parser.add_argument('--episodic-replay', action='store_true', default=False)
+    parser.add_argument('--replay-start-size', type=int, default=1000)
+    parser.add_argument('--target-update-interval', type=int, default=10 ** 2)
+    parser.add_argument('--target-update-method', type=str, default='hard')
+    parser.add_argument('--soft-update-tau', type=float, default=1e-2)
+    parser.add_argument('--update-interval', type=int, default=1)
+    parser.add_argument('--eval-n-runs', type=int, default=50)
+    parser.add_argument('--eval-interval', type=int, default=10 ** 3)
+    parser.add_argument('--n-hidden-channels', type=int, default=512)
+    parser.add_argument('--n-hidden-layers', type=int, default=2)
+    parser.add_argument('--gamma', type=float, default=0.99)
+    parser.add_argument('--minibatch-size', type=int, default=None)
+    parser.add_argument('--maxturns', type=int, default=80)
+    args = parser.parse_args()
 
-model_saved_name = args.model_name
-rounds = args.rounds
-agent_method = create_ddqn_agent
+    # test
+    if not args.test:
+        print("training...")
+        args.outdir = experiments.prepare_output_dir(
+            args, args.outdir, argv=sys.argv)
+        print('Output files are saved in {}'.format(args.outdir))
 
-model = "{}{}_{}".format(model_dir, model_saved_name, rounds)
-test_result = "{}{}_{}/scores.txt".format(model_dir, model_saved_name, rounds)
+        env, agent = train_agent(args)
 
-if not os.path.exists(model):
-    os.makedirs(model)
+        with open(os.path.join(args.outdir, 'scores.txt'), 'w') as f:
+            f.write("total_turn/episode->{}({}/{})\n".format(env.total_turn / env.episode, env.total_turn, env.episode))
+    else:
+        print("testing...")
+        total = len(sha256_holdout)
+        fe = pefeatures.PEFeatureExtractor()
 
-# test
-if not args.test:
-    print("training...")
-    env, agent = train_agent(rounds=int(rounds), use_score=False, name=model, create_agent=agent_method,
-                             use_gpu=args.gpu)
-    with open(test_result, 'a+', encoding='utf-8') as f:
-        f.write("total_turn/episode->{}({}/{})\n".format(env.total_turn / env.episode, env.total_turn, env.episode))
-else:
-    print("testing...")
-    total = len(sha256_holdout)
-    fe = pefeatures.PEFeatureExtractor()
+        def agent_policy(agent):
+            def f(bytez):
+                # first, get features from bytez
+                feats = fe.extract(bytez)
+                action_index = agent.act(feats)
+                return ACTION_LOOKUP[action_index]
 
+            return f
 
-    def agent_policy(agent):
-        def f(bytez):
-            # first, get features from bytez
-            feats = fe.extract(bytez)
-            action_index = agent.act(feats)
-            return ACTION_LOOKUP[action_index]
-
-        return f
-
-    # ddqn
-    env = gym.make('malware-test-v0')
-    agent = agent_method(env)
-    mm = get_latest_model_dir_from(model)
-    agent.load(mm)
-    success, _ = evaluate(agent_policy(agent))
-    blackbox_result = "black: {}({}/{})".format(len(success) / total, len(success), total)
-    with open(test_result, 'a+') as f:
-        f.write("result->{}\n".format(blackbox_result))
+        # ddqn
+        env = gym.make('malware-test-v0')
+        agent = create_ddqn_agent(env, args)
+        mm = get_latest_model_dir_from(args.outdir)
+        agent.load(mm)
+        success, _ = evaluate(agent_policy(agent))
+        blackbox_result = "black: {}({}/{})".format(len(success) / total, len(success), total)
+        with open(os.path.join(args.outdir, 'scores.txt'), 'w') as f:
+            f.write("{}->{}\n".format(mm, blackbox_result))
