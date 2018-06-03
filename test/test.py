aaf74442a161d379e7ed1bbecc739218eb7c0267
fangzhiyang <fanmcgrady@gmail.com>
2018-06-04 00:04:20 +0800

    完美的模型
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- test/test.py
+++ test/test.py
@@ -1,10 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
+from hook.training_scores_hook import TrainingScoresHook
 
-x = np.linspace(0, 2 * np.pi, 10)
-y = np.sin(x)
-xvals = np.linspace(0, 2 * np.pi, 50)
-yinterp = np.interp(xvals, x, y)
-plt.plot(x, y, 'o')
-plt.plot(xvals, yinterp, '-x')
-plt.show()
+hook = TrainingScoresHook('text', '')
+hook(None, None, 0)
