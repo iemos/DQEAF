a390cacc829a60c0660be50ba730842866313f71
fangzhiyang <fanmcgrady@gmail.com>
2018-06-08 19:18:25 +0800

    steps=1200 防止失败的情况下浪费时间 rounds=10 训练提前结束，标识成功
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- test/test.py
+++ test/test.py
@@ -1,7 +1,11 @@
 # 标识成功失败
 import os
 
-dirs = os.listdir('logs')
-# 10个txt+2个目录
-if len(dirs) == 9:
-    os.rename('logs', 'logs-success')
+# 标识成功失败
+dirs = os.listdir('../models/20180603T153624.120380')
+# 训练提前结束，标识成功
+for mm in dirs:
+    # mm = str(file)
+    print(mm)
+    if mm.endswith('_finish') and not mm.startswith('1'):
+        print(mm)
