6a15e5bd0c231ee4c8c7ec2a26277c0e6378b26b
fangzhiyang <fanmcgrady@gmail.com>
2018-06-08 19:19:01 +0800

    steps=1200 防止失败的情况下浪费时间 rounds=10 训练提前结束，标识成功
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- test/test.py
+++ test/test.py
@@ -2,7 +2,7 @@
 import os
 
 # 标识成功失败
-dirs = os.listdir('../models/20180603T153624.120380')
+dirs = os.listdir('../models/20180608T133427.173935')
 # 训练提前结束，标识成功
 for mm in dirs:
     # mm = str(file)
