9e68485484771dcd96b2c571f97b8a7214b26faa
fangzhiyang <fanmcgrady@gmail.com>
2018-06-09 10:50:34 +0800

    网络[512,64]
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- train.py
+++ train.py
@@ -58,7 +58,7 @@ def main():
         def __init__(self, obs_size, n_actions, n_hidden_channels=None):
             super(QFunction, self).__init__()
             if n_hidden_channels is None:
-                n_hidden_channels = [1024, 1024]
+                n_hidden_channels = [512, 64]
             net = []
             inpdim = obs_size
             for i, n_hid in enumerate(n_hidden_channels):
