054ddc169df8231b7b55512789ab41954ebb4377
fangzhiyang <fanmcgrady@gmail.com>
2018-08-02 14:08:32 +0800

    30000 steps
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- train.py
+++ train.py
@@ -35,7 +35,7 @@ def main():
     parser.add_argument('--start-epsilon', type=float, default=1.0)
     parser.add_argument('--end-epsilon', type=float, default=0.1)
     parser.add_argument('--load', type=str, default=None)
-    parser.add_argument('--steps', type=int, default=9100)
+    parser.add_argument('--steps', type=int, default=30000)
     parser.add_argument('--prioritized-replay', action='store_false')
     parser.add_argument('--episodic-replay', action='store_true')
     parser.add_argument('--replay-start-size', type=int, default=1000)
@@ -55,7 +55,8 @@ def main():
         def __init__(self, obs_size, n_actions, n_hidden_channels=None):
             super(QFunction, self).__init__()
             if n_hidden_channels is None:
-                n_hidden_channels = [1024, 256]
+                n_hidden_channels = [512, 128]
+                # n_hidden_channels = [1024, 256]
             net = []
             inpdim = obs_size
             for i, n_hid in enumerate(n_hidden_channels):
