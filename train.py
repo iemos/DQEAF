09d122a1b054f29c92859b5a2261ae9e94fd9efe
fangzhiyang <fanmcgrady@gmail.com>
2018-08-12 19:55:47 +0800

    dropout
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

--- train.py
+++ train.py
@@ -35,7 +35,7 @@ def main():
     parser.add_argument('--start-epsilon', type=float, default=1.0)
     parser.add_argument('--end-epsilon', type=float, default=0.1)
     parser.add_argument('--load', type=str, default=None)
-    parser.add_argument('--steps', type=int, default=20000)
+    parser.add_argument('--steps', type=int, default=30000)
     parser.add_argument('--prioritized-replay', action='store_false')
     parser.add_argument('--episodic-replay', action='store_true')
     parser.add_argument('--replay-start-size', type=int, default=1000)
@@ -62,7 +62,7 @@ def main():
                 net += [('l{}'.format(i), L.Linear(inpdim, n_hid))]
                 # net += [('norm{}'.format(i), L.BatchNormalization(n_hid))]
                 net += [('_act{}'.format(i), F.tanh)]
-                # net += [('_dropout{}'.format(i), F.dropout)]
+                net += [('_dropout{}'.format(i), F.dropout)]
                 inpdim = n_hid
 
             net += [('output', L.Linear(inpdim, n_actions))]
