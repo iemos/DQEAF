48e0da1b229f586694bc84d976abcb44d2c3cd5b
fangzhiyang <fanmcgrady@gmail.com>
2018-08-25 23:07:37 +0800

    参数
    Signed-off-by: fangzhiyang <fanmcgrady@gmail.com>

---
+++ test/sample-test.py
@@ -0,0 +1,21 @@
+# gym_malware interface hello world
+import os
+import sys
+
+sys.path.append("..")
+from gym_malware.envs.utils import interface
+
+# 统计sample里样本组成情况
+sha_list = interface.get_available_sha256()
+malware = []
+benign = []
+for sha256 in sha_list:
+    bytez = interface.fetch_file(sha256)
+    label = interface.get_label_local(bytez)
+    if label == 0.0:
+        benign.append(sha256)
+        interface.delete_file(sha256)
+    else:
+        malware.append(sha256)
+
+print('malware:{}, benign:{}'.format(malware.__len__(), benign.__len__()))
