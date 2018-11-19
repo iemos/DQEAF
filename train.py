9e396d6e2486bb7a1d4029bef2727401530cd7b9
fangzhiyang <fanmcgrady@gmail.com>
2018-11-19 23:56:13 +0800

    使用gpu

--- train.py
+++ train.py
@@ -105,7 +105,8 @@ def main():
         #     n_hidden_layers=args.n_hidden_layers)
         q_func = QFunction(obs_size, n_actions)
         if args.gpu:
-            q_func.to_gpu(args.gpu)
+            # 使用第一个gpu
+            q_func.to_gpu(0)
 
         # Draw the computational graph and save it in the output directory.
         if not args.test and not args.gpu:
