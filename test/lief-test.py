29b4599a972e590e60762f3e8a5bb83db49c48c1
jsllby <857700847@qq.com>
2018-11-20 13:22:42 +0800

    Update lief-test.py

--- test/lief-test.py
+++ test/lief-test.py
@@ -1,12 +1,29 @@
 import pefile
-
+import lief
 from gym_malware.envs.utils import interface
 
 # 从bytez字节parse，该方法只能使用lief 0.7版本
 # Mac系统下使用0.8要报错，原因未知
 from gym_malware.envs.utils.extractPE import extractPE
 
-byte = interface.fetch_file("Backdoor.Win32.Hupigon.zay");
+
+
+# byte = interface.fetch_file('Worm.Win32.AutoRun.sbf', test=True)
 # binary = lief.PE.parse(byte)
 # print(binary)
-print(extractPE().createFeature(byte))
+# print(extractPE().createFeature(byte))
+
+
+sha_list = interface.get_available_sha256()
+normal = []
+unnormal = []
+for sha256 in sha_list:
+    bytez = interface.fetch_file(sha256)
+    try:
+        binary = lief.PE.parse(bytez)
+        normal.append(sha256)
+    except:
+        unnormal.append(sha256)
+        interface.delete_file(sha256)
+
+print('normal:{}, unnormal:{}'.format(normal.__len__(), unnormal.__len__()))
